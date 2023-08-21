df56b6fdc0cce94baf910ab2b5b8a8fcd0863fe1
kapilagrawal393 <kapilagrawal393@gmail.com>
2023-08-21 09:32:30 +0530

    kuldeep's new task

--- src/restAssured/RestAssuredCode.java
+++ src/restAssured/RestAssuredCode.java
@@ -6,14 +6,16 @@ public class RestAssuredCode
 	{
 		System.out.println("Hi I am rest assured 1");
 	}
-	
 	public void restAssured2()
 	{
 		System.out.println("Kuldeep's work");
 	}
-	
 	public void restAssured3()
 	{
 		System.out.println("Kapil's work");
+	}
+	public void restAssured4()
+	{
+		System.out.println("Kuldeep's new work");
 	}
 }
