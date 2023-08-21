52bf409050f2de99d37862b18065bc1176e10986
skuldeepsingh099 <skuldeepsingh099@gmail.com>
2023-08-21 08:33:59 +0530

    first commit

---
+++ src/restAssured/RestAssuredCode.java
@@ -0,0 +1,9 @@
+package restAssured;
+
+public class RestAssuredCode 
+{
+	public void restAssured1()
+	{
+		System.out.println("Hi I am rest assured 1");
+	}
+}
---
+++ src/selenium/SeleniumCode.java
@@ -0,0 +1,9 @@
+package selenium;
+
+public class SeleniumCode 
+{
+	public void selenium1()
+	{
+		System.out.println("Hi I am selenium 1");
+	}
+}
